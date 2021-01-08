1e97dc198f6118306427f6336e0c1c3d787cf865
RachelNS <rachelnelsonschille@gmail.com>
2021-01-08 15:54:42 -0800

    write file to new README.md

--- index.js
+++ index.js
@@ -1,11 +1,112 @@
 const inqiurer = require(`./node_modules/inquirer`);
 const fs = require(`fs`);
 
-inquirer
+inqiurer
     .prompt([
         {
             type: `input`,
             name: `projectTitle`,
             message: `What is the title of your project?`
+        },
+        {
+            type: `input`,
+            name: `projectDescription`,
+            message: `Please describe your project.`
+        },
+        {
+            type: `input`,
+            name: `projectInstallation`,
+            message: `What are the installation instructions for your project?`
+        },
+        {
+            type: `input`,
+            name: `projectUsage`,
+            message: `Please describe how to use your project.`
+        },
+        {
+            type: `checkbox`,
+            name: `projectLicense`,
+            message: `Please select your project's license.`,
+            choices: [
+                { name: `MIT` }
+            ]
+        },
+        {
+            type: `input`,
+            name: `projectContribution`,
+            message: `What are the contribution guidelines for your project?`
+        },
+        {
+            type: `input`,
+            name: `projectTest`,
+            message: `Please describe how to test your project.`
+        },
+        {
+            type: `input`,
+            name: `userGithub`,
+            message: `What is your Github Username?`
+        },
+        {
+            type: `input`,
+            name: `userEmail`,
+            message: `What is your email address?`
         }
-    ])
+
+
+    ]).then((response) => {
+let readmeString = 
+`# ${response.projectTitle}
+
+[![License: ${response.projectLicense}](https://img.shields.io/badge/License-${response.projectLicense}-yellow.svg)](https://opensource.org/licenses/${response.projectLicense})
+
+## Description
+
+${response.projectDescription}
+
+## Table of Contents
+
+[Installation](##Installation)
+
+[Usage](##Usage)
+
+[License](##License)
+
+[Contributing](##Contributing)
+
+[Tests](##Tests)
+
+[Questions/Contact](##Questions)
+
+## Installation
+
+${response.projectInstallation}
+
+## Usage
+
+${response.projectUsage}
+
+## License
+
+This application is covered under a(n) ${response.projectLicense} License.
+
+## Contributing
+
+${response.projectContribution}
+
+## Tests
+
+${response.projectTest}
+
+## Questions
+
+[Github Profile](https://github.com/${response.userGithub})
+
+Or, email me with addition questions at ${response.userEmail}.`
+
+        fs.writeFile("README.md", readmeString, (err) =>
+            err ? console.error(err) : console.log("Success!"))
+
+
+    }).catch(function (err) {
+        err ? console.error(err) : console.log("Success!")
+    })
--- template.md
+++ template.md
@@ -1,17 +1,47 @@
 # ${response.projectTitle}
 
+[![License: ${response.projectLicense}](https://img.shields.io/badge/License-${response.projectLicense}-yellow.svg)](https://opensource.org/licenses/${response.projectLicense})
+
 ## Description
+
+${response.projectDescription}
 
 ## Table of Contents
 
+[Installation](##Installation)
+
+[Usage](##Usage)
+
+[License](##License)
+
+[Contributing](##Contributing)
+
+[Tests](##Tests)
+
+[Questions/Contact](##Questions)
+
 ## Installation
+
+${response.projectInstallation}
 
 ## Usage
 
+${response.projectUsage}
+
 ## License
+
+This application is covered under a(n) ${response.projectLicense} License.
 
 ## Contributing
 
+${response.projectContribution}
+
 ## Tests
 
+${response.projectTest}
+
 ## Questions
+
+[Github Profile](https://github.com/${userGithub})
+
+Or, email me with addition questions at ${response.userEmail}.
